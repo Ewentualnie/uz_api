9431c711528b1e3b998b312b982e8c6f50d5c3f2
Serhii Zaika <ewentualnie.sz@gmail.com>
2025-06-07 14:33:54 +0300

    add validations

--- src/station/dto/create-station.dto.ts
+++ src/station/dto/create-station.dto.ts
@@ -1,7 +1,16 @@
-import { IsString, Matches, MinLength, IsInt } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsString,
+  Matches,
+  MinLength,
+  IsInt,
+  IsNotEmpty,
+} from 'class-validator';
 
 export class CreateStationDto {
+  @ApiProperty({ example: 'Kyiv', description: 'Назва станції' })
   @IsString()
+  @IsNotEmpty({ message: "Назва станції є обов'язковою" })
   @MinLength(3, { message: 'Назва станції має містити щонайменше 3 символи' })
   @Matches(/^[А-Яа-яЇїІіЄєҐґ0-9'’\- ]+$/, {
     message:
@@ -9,6 +18,8 @@ export class CreateStationDto {
   })
   name: string;
 
+  @ApiProperty({ example: '2200000', description: 'Код станції' })
+  @IsNotEmpty({ message: "Код станції є обов'язковим" })
   @IsInt({ message: 'Код станції має бути цілим числом' })
   @Matches(/^22\d{5}$/, {
     message: 'Код станції - це 7-значне число, що починається з 22.',
