e6e17050bc2d83b15f5f94ed5bf0e579c2244d76
Serhii Zaika <ewentualnie.sz@gmail.com>
2025-06-07 16:12:07 +0300

    remove id in db response

--- src/station/entities/station.entity.ts
+++ src/station/entities/station.entity.ts
@@ -1,1 +1,8 @@
-export class Station {}
+import { Exclude } from 'class-transformer';
+
+export class Station {
+  @Exclude()
+  id: number;
+  name: string;
+  code: number;
+}
--- src/station/station.service.ts
+++ src/station/station.service.ts
@@ -7,6 +7,8 @@ import {
 import { CreateStationDto } from './dto/create-station.dto';
 import { Pool } from 'pg';
 import { S_Queries } from 'src/database/database.queries';
+import { Station } from './entities/station.entity';
+import { plainToInstance } from 'class-transformer';
 
 @Injectable()
 export class StationService {
@@ -23,7 +25,7 @@ export class StationService {
         name,
         code,
       ]);
-      return result.rows[0];
+      return plainToInstance(Station, result.rows[0]);
     } catch (error) {
       if (error.code === '23505') {
         throw new ConflictException('Така станція вже існує');
@@ -39,11 +41,17 @@ export class StationService {
 
   async findAll() {
     const result = await this.pool.query(S_Queries.getAllStations);
-    return result.rows;
+    return plainToInstance(Station, result.rows);
   }
 
   async findOne(code: number) {
     const result = await this.pool.query(S_Queries.getStationByCode, [code]);
-    return result.rows[0];
+    if (result.rows[0]) {
+      return result.rows[0].name;
+    } else {
+      throw new BadRequestException(
+        'Станції з таким кодом не існує, або ще не додана',
+      );
+    }
   }
 }
